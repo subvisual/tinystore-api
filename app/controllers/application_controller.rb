ce46d9326e0a512decabc5fcbc973b03bb4cea26
Zamith <zamith.28@gmail.com>
2013-12-31 03:06:43 +0000

    Allows the connection of the express app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::API
   after_filter :set_access_control_headers
 
   def set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = 'null'
+    headers['Access-Control-Allow-Origin'] = 'http://localhost:3001'
     headers['Access-Control-Request-Method'] = '*'
   end
 end
